3462044155b413ad5a22a202c563ce4fd86599ab
Hubert Kecler <hubert.kecler@edu.itspiemonte.it>
2023-12-13 20:04:52 +0100

    ho aggiunto un'animazione alla navbar in versione mobile

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,7 +7,6 @@ import { Component } from "@angular/core";
 })
 export class NavbarComponent {
     isMenuOpen: boolean = false;
-
     toggleMenu() {
         this.isMenuOpen = !this.isMenuOpen;
     }
