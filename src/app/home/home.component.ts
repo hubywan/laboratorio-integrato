5e7ae1c12453ce33a5e096b95d44636c7b5cde84
Hubert Kecler <hubert.kecler@edu.itspiemonte.it>
2023-12-14 12:58:10 +0100

    ho creato dei mock e collegato home page ad articoli giusto per provare

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,26 @@
 import { Component } from "@angular/core";
+import { OnInit } from "@angular/core";
+import { MockDataService } from "src/services/mock-service";
 
 @Component({
     selector: "app-home",
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+export class HomeComponent {
+    items: any;
+
+    constructor(private mockDataService: MockDataService) {}
+
+    ngOnInit(): void {
+        this.mockDataService.getMockData().subscribe(
+            (data) => {
+                this.items = data.articoli;
+                console.log("Dati mock ottenuti:", this.items);
+            },
+            (error) => {
+                console.error("Errore durante il recupero dei dati:", error);
+            }
+        );
+    }
+}
