535cbc0407194fda0108c5f44f0c17ef70d901e7
Hubert Kecler <hubert.kecler@edu.itspiemonte.it>
2024-01-07 14:57:42 +0100

    iniziato dati in piu con grafico in classifica piloti per ora on successo :)

--- src/app/classifica-piloti/classifica-piloti.component.ts
+++ src/app/classifica-piloti/classifica-piloti.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { ApiService } from "src/services/api.service";
 import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-classifica-piloti",
@@ -10,16 +11,20 @@ import { ActivatedRoute } from "@angular/router";
 export class ClassificaPilotiComponent {
     items: any;
     selectedYear: number = 2023;
+    id: number = 1;
+    showMoreData: boolean[] = [];
 
     constructor(
         private apiservice: ApiService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private router: Router
     ) {}
 
     ngOnInit(): void {
         this.route.queryParams.subscribe((params) => {
             this.selectedYear = +params["anno"] || this.selectedYear;
             this.fetchDataForYear(this.selectedYear);
+            this.initializeShowMoreData();
         });
     }
     fetchDataForYear(year: number): void {
@@ -37,5 +42,18 @@ export class ClassificaPilotiComponent {
                 console.error("Errore durante il recupero dei dati:", error);
             }
         );
+    }
+    redirectToDettaglioPilota(id: number): void {
+        this.router.navigate(["/dettaglio-pilota"], {
+            queryParams: { id },
+        });
+    }
+    // Metodo per inizializzare l'array showSecondDiv
+    initializeShowMoreData() {
+        this.showMoreData = this.items.map(() => false);
+    }
+
+    toggleMoreData(index: number) {
+        this.showMoreData[index] = !this.showMoreData[index];
     }
 }
